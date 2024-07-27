365722dad4d3f0b370a85c281fcf7a02d4bbdbc4
1ui1s <doluis2006@gmail.com>
2024-07-26 23:07:59 -0700

    added test html and api for get and post

--- server/test.js
+++ server/test.js
@@ -1,18 +1,62 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 dotenv.config();
-
-const { MongoClient } = require("mongodb");
-
 
 // ur url in .env file
 const uri = process.env.uri
 const port = process.env.port
 
-const client = new MongoClient(uri);
+const app = express();
+const Test = require("./models/testModel");
+
+app.use(express.json());
+app.use(cors());
+
+// connect to mongoDB & start server
+mongoose.connect(uri)
+  .then(() => {
+    console.log('MongoDB connected')
+  })
+  .catch((err) => {
+    console.log(err)
+  });
+
+// Default route to test connection to the server
+app.get("/", async (req, res) => {
+  res.send("Welcome");
+});
 
 
+// GET all users from mongoDB
+app.get("/user", async (req, res) => {
+  try {
+    const docs = await Test.find();
+    res.status(200).json(docs);
+  } catch (error) {
+    res.status(500).json({error: "Error getting user documents"})
+  }
+});
+
+app.post("/user", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const doc = new Test({ name, email, password});
+    const saveDoc = await doc.save();
+    res.status(201).json({_id: saveDoc._id });
+  } catch (error) {
+    res.status(500).json({error: "error adding user"});
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}!`)
+})
+
+//const client = new MongoClient(uri);
+//const { MongoClient } = require("mongodb");
 //run this to check if your creditionals are right
 /*
 async function run() {
@@ -28,8 +72,6 @@ async function run() {
     }
   }
   run().catch(console.dir);
-
-
 
 
 */
@@ -50,9 +92,9 @@ const database = client.db(db);
 const c = process.env.collection;
 const coll = database.collection(c)
  
-const doc = { "name": "Dan Daniel Dan", "email": "dandandan@dan.com", "password":"notdan"};
+const doc = { "name": "luis peach", "email": "luispeach@gmail.com", "password":"luispeach"};
 
 var result = coll.insertOne(doc);
 
-console.log('A document was isnerted with the _id: ${result.insertedId}');
+console.log(`A document was inserted with the _id: ${result.insertedId}`);
 */
